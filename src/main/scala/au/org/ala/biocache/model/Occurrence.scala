18ebf3440b8588d9f7e7d947ada486a0193f0f38
Helen Manders-Jones <realhelenmjones@gmail.com>
2023-04-13 09:03:08 +0100

    added accesscontrol fields

--- src/main/scala/au/org/ala/biocache/model/Occurrence.scala
+++ src/main/scala/au/org/ala/biocache/model/Occurrence.scala
@@ -117,8 +117,35 @@ class Occurrence extends Cloneable with POSO {
   @BeanProperty var outlierForLayers:Array[String] = _
   @BeanProperty var photographer:String =_
 
+  //Access-Control
+  @BeanProperty var publicResolutionInMeters:String = _
+
+
+  //Store the original values before the public resolution blurring is applied
+  var suppliedAccessControlledValues:Map[String,String] = _
+
+
   @JsonIgnore
   def getOriginalSensitiveValues():Map[String,String] = originalSensitiveValues
 
   def setOriginalSensitiveValues(originalSensitiveValues:Map[String,String]) = this.originalSensitiveValues = originalSensitiveValues
+
+  @JsonIgnore
+  def getSuppliedAccessControlledValues():Map[String,String] = suppliedAccessControlledValues
+  def setSuppliedAccessControlledValues(suppliedAccessControlledValues:Map[String,String]) = this.suppliedAccessControlledValues = suppliedAccessControlledValues
+
+  //Store the loaded values before they are cleared prior to record upsert
+  var loadedAccessControlledValues:Map[String,String] = _
+
+  @JsonIgnore
+  def getLoadedAccessControlledValues():Map[String,String] = loadedAccessControlledValues
+  def setLoadedAccessControlledValues(loadedAccessControlledValues:Map[String,String]) = this.loadedAccessControlledValues = loadedAccessControlledValues
+
+  var publicResolutionToBeAppliedInMeters:String = _
+
+  @JsonIgnore
+  def getPublicResolutionToBeAppliedInMeters():String = publicResolutionToBeAppliedInMeters
+  def setPublicResolutionToBeAppliedInMeters(publicResolutionToBeAppliedInMeters:String) = this.publicResolutionToBeAppliedInMeters = publicResolutionToBeAppliedInMeters
+
+
 }
